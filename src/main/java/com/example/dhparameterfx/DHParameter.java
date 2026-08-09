39db3d054eaf02365ccf7262503f51c4bf141bd9
Gil-gil-glitch <gilsocojp@gmail.com>
2026-08-10 02:41:37 +0900

    Updated so that theta and angles are appropriately in radians to handle the DH mistmatch

--- src/main/java/com/example/dhparameterfx/DHParameter.java
+++ src/main/java/com/example/dhparameterfx/DHParameter.java
@@ -2,19 +2,22 @@ package com.example.dhparameterfx;
 
 public record DHParameter(double a, double alpha, double d, double theta) {
 
-    public Matrix4x4 toTransformMatrix(){
+    public Matrix4x4 toTransformMatrix() {
+
+        double radTheta = Math.toRadians(theta); // Handles Angles in Radians now, instead of degrees
+        double radAlpha = Math.toRadians(alpha);
 
         // parameters
-        double cosT = Math.cos(theta);
-        double sinT = Math.sin(theta);
-        double cosA = Math.cos(alpha);
-        double sinA = Math.sin(alpha);
+        double cosT = Math.cos(radTheta);
+        double sinT = Math.sin(radTheta);
+        double cosA = Math.cos(radAlpha);
+        double sinA = Math.sin(radAlpha);
 
         return new Matrix4x4(new double[][]{
-            { cosT, -sinT * cosA, sinT * sinA, a * cosT},
-            { sinT, cosT * cosA, -cosT * sinA, a * sinT},
-            {0, sinA, cosA, d, },
-            {0, 0, 0, 1}
+                { cosT, -sinT * cosA,  sinT * sinA, a * cosT },
+                { sinT,  cosT * cosA, -cosT * sinA, a * sinT },
+                {     0,        sinA,         cosA,        d },
+                {     0,           0,            0,        1 }
         });
     }
 }
